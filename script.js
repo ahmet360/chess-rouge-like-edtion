d74e9961fd255bb28e9611b8389f350b8932c5c7
ahmet360 <93016308+ahmet360@users.noreply.github.com>
2023-03-09 17:58:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,60 @@ const pieces = [
   { name: "pawn", symbol: "♟", color: "black" },
   { name: "pawn", symbol: "♟", color: "black" },
 ];
+const board = document.getElementById("board");
+
+for (let i = 0; i < 64; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  if ((i + Math.floor(i / 8)) % 2 === 0) {
+    square.classList.add("light");
+  } else {
+    square.classList.add("dark");
+  }
+  board.appendChild(square);
+}
+for (let i = 0; i < pieces.length; i++) {
+  const piece = document.createElement("div");
+  piece.classList.add("piece");
+  piece.innerHTML = pieces[i].symbol;
+  piece.setAttribute("draggable", "true");
+  piece.dataset.color = pieces[i].color;
+  piece.dataset.name = pieces[i].name;
+  const square = document.querySelector(`.square[data-index="${i}"]`);
+  square.appendChild(piece);
+}
+let draggedPiece = null;
+
+document.addEventListener("dragstart", function (event) {
+  const target = event.target;
+  if (target.classList.contains("piece")) {
+    draggedPiece = target;
+  }
+});
+
+document.addEventListener("dragover", function (event) {
+  event.preventDefault();
+});
+
+document.addEventListener("drop", function (event) {
+  const target = event.target;
+  if (target.classList.contains("square")) {
+    const color = draggedPiece.dataset.color;
+    const name = draggedPiece.dataset.name;
+    const fromIndex = parseInt(draggedPiece.parentNode.dataset.index);
+    const toIndex = parseInt(target.dataset.index);
+    const legalMove = isLegalMove(color, name, fromIndex, toIndex);
+    if (legalMove) {
+      const capturedPiece = target.querySelector(".piece");
+      if (capturedPiece !== null) {
+        capturedPiece.parentNode.removeChild(capturedPiece);
+      }
+      target.appendChild(draggedPiece);
+    }
+  }
+  draggedPiece = null;
+});
+function isLegalMove(color, name, fromIndex, toIndex) {
+  // Implement game logic here
+  return true; // Default to allowing any move
+}
